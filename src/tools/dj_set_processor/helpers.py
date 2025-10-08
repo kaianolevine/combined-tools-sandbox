f4960d8e647d382e0a0c6ef76e923a5a388941d6
Kaiano Levine <kaiano.levine@gmail.com>
2025-10-07 23:02:35 -0500

    Fixing local include path for testing

--- src/tools/dj_set_processor/helpers.py
+++ src/tools/dj_set_processor/helpers.py
@@ -3,7 +3,7 @@
 import time
 import re
 
-import google_api
+import tools.dj_set_processor.google_api as google_api
 import tools.dj_set_processor.config as config
 from difflib import SequenceMatcher
 from googleapiclient.errors import HttpError
